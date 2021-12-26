632d37a6da773f113addff55dcd8dc983bd30701
eclipse7723 <eclipse7723@gmail.com>
2021-12-26 04:30:29 +0200

    fix encoding

--- lab2/query.sql
+++ lab2/query.sql
@@ -1,4 +1,4 @@
--- РІРёРІРµСЃС‚Рё РЅР°Р·РІСѓ РїРёРІР°, Р±СЂРѕРІР°СЂРЅС–, СЃРѕСЂС‚, РјС–С†РЅС–СЃС‚СЊ С‚Р° РјС–СЃС‚Рѕ РІРёСЂРѕР±РЅРёРєР°
+-- вивести назву пива, броварні, сорт, міцність та місто виробника
 SELECT b.name AS "Beer", s.name AS "Style", bb.name AS "Brewery",
        b.abv AS "Alcohol by volume", loc.city AS "City"
 	FROM Beers AS b
@@ -6,18 +6,18 @@ SELECT b.name AS "Beer", s.name AS "Style", bb.name AS "Brewery",
         LEFT JOIN Locations AS loc	ON bb.loc_id = loc.loc_id
         LEFT JOIN Styles AS s	ON b.style_id = s.style_id;
 
--- 3Р° - СЂРѕР·РїРѕРґС–Р»РµРЅРЅСЏ РјС–С†РЅРѕСЃС‚С– РїРёРІР°;
+-- 3а - розподілення міцності пива;
 SELECT COUNT(b.abv) AS "Count", b.abv AS "Alcohol by volume"
 	FROM Beers AS b GROUP BY b.abv ORDER BY COUNT(b.abv) DESC;
 
--- 3Р± - РєС–Р»СЊРєС–СЃС‚СЊ РІРЅРµСЃРµРЅРѕРіРѕ РїРёРІР° РІ РєРѕР¶РЅРѕРјСѓ РјС–СЃС‚С–;
+-- 3б - кількість внесеного пива в кожному місті;
 SELECT COUNT(loc.city) as "Count", loc.city as "City"
     FROM Beers as b
         LEFT JOIN Breweries AS bb	ON b.brewery_id = bb.brewery_id
         LEFT JOIN Locations AS loc	ON bb.loc_id = loc.loc_id
     GROUP BY loc.city ORDER BY COUNT(loc.city) DESC;
 
--- 3РІ. Р·Р°Р»РµР¶РЅС–СЃС‚СЊ РјС–С†РЅРѕСЃС‚С– РїРёРІР° РІС–Рґ СЃРѕСЂС‚Сѓ (СЃС‚РёР»СЊ)
+-- 3в. залежність міцності пива від сорту (стиль)
 SELECT COUNT(b.abv) AS "Count", ROUND(AVG(b.abv)::numeric, 2) AS "Average alcohol by volume", s.name AS "Style"
 	FROM Beers AS b LEFT JOIN Styles AS s	ON b.style_id = s.style_id
 	GROUP BY s.style_id ORDER BY COUNT(b.abv) DESC;
