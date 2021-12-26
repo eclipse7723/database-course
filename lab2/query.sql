a62757047b4cb7a0f295be7bfc81bdedbadf6ebc
eclipse7723 <eclipse7723@gmail.com>
2021-12-26 03:54:07 +0200

    some queries

---
+++ lab2/query.sql
@@ -0,0 +1,23 @@
+-- вивести назву пива, броварні, сорт, міцність та місто виробника
+SELECT b.name AS "Beer", s.name AS "Style", bb.name AS "Brewery",
+       b.abv AS "Alcohol by volume", loc.city AS "City"
+	FROM Beers AS b
+        LEFT JOIN Breweries AS bb	ON b.brewery_id = bb.brewery_id
+        LEFT JOIN Locations AS loc	ON bb.loc_id = loc.loc_id
+        LEFT JOIN Styles AS s	ON b.style_id = s.style_id;
+
+-- 3а - розподілення міцності пива;
+SELECT COUNT(b.abv) AS "Count", b.abv AS "Alcohol by volume"
+	FROM Beers AS b GROUP BY b.abv ORDER BY COUNT(b.abv) DESC;
+
+-- 3б - кількість внесеного пива в кожному місті;
+SELECT COUNT(loc.city) as "Count", loc.city as "City"
+    FROM Beers as b
+        LEFT JOIN Breweries AS bb	ON b.brewery_id = bb.brewery_id
+        LEFT JOIN Locations AS loc	ON bb.loc_id = loc.loc_id
+    GROUP BY loc.city ORDER BY COUNT(loc.city) DESC;
+
+-- 3в. залежність міцності пива від сорту (стиль)
+SELECT COUNT(b.abv) AS "Count", ROUND(AVG(b.abv)::numeric, 2) AS "Average alcohol by volume", s.name AS "Style"
+	FROM Beers AS b LEFT JOIN Styles AS s	ON b.style_id = s.style_id
+	GROUP BY s.style_id ORDER BY COUNT(b.abv) DESC;
