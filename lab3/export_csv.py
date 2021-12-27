36a5ecacbb3fef114c96e4258bed1258350f5153
eclipse7723 <eclipse7723@gmail.com>
2021-12-27 05:44:59 +0200

    export from db to local csv

---
+++ lab3/export_csv.py
@@ -0,0 +1,20 @@
+from lab2.main import login
+import csv
+
+
+tables = ["Beers", "Breweries", "Locations",
+          "Styles", "Types", "Breweries_Types"]
+
+
+with login() as connect:
+    cursor = connect.cursor()
+
+    for table_name in tables:
+        cursor.execute("SELECT * FROM %s" % table_name)
+
+        columns = [col[0] for col in cursor.description]
+        with open("export\\%s.csv" % table_name, "w", encoding='cp850', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(columns)
+            for row in cursor:
+                writer.writerow(row)
